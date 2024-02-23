d3999e34d3ca90c773a781cc08d314f34f396abe
ChetasA <887566adu@ames.k12.ia.us>
2024-02-22 18:51:32 -0600

    JS

--- Jackson/ScoutingApp24.js
+++ Jackson/ScoutingApp24.js
@@ -5,6 +5,12 @@ var teamNumber = []; //Team Number
 var actionList = ["Red Alliance"]; //This is the list that populates the log with human friendly text.
 var compressedList = []; //This is the list that collects all the IDs for the QR Code.
 var comments = ""; //Comments Box
+var blue1 = [];
+var blue2 = [];
+var blue3 = [];
+var red1 = [];
+var red2 = [];
+var red3 = [];
 var ipadID = sessionStorage.getItem("iPadId");
 let savedComments = [];
 var incmatchnumber = sessionStorage.getItem("matchnumber");
@@ -15,14 +21,11 @@ var curentAction = "";
 var MakeqrExtraData = [];
 var MakeqrCompList = [];
 var rUsure = "";
-
-const TeamNumbers = require('./TeamNumbersArray.js')
-
 /* Function List
 --- Direct Button Functions ---
 changeMatchNumber: Used to change the match number
 changeTeamNumber: Used to change the team number
-: Called everytime a button is pushed.
+addAction: Called everytime a button is pushed.
 Undo: Pops items off of all the lists.
 moveTableLeft: Used to move backwards in the table structure
 moveTableRight: Used to move forward in the table structure
@@ -40,9 +43,9 @@ Combined lowerCounter and raiseCounter functions into the updateAvail function t
 
 
 // ---- DIRECT BUTTON FUNCTIONS ---
-function mobility() {
-  var mob = actionList.indexOf("Mobility");
-  var comMob = compressedList.indexOf(11);
+function leave() {
+  var mob = actionList.indexOf("Leave");
+  var comMob = compressedList.indexOf(20);
 
   if (mob > -1) {
     actionList.splice(mob, 1);
@@ -52,72 +55,11 @@ function mobility() {
   }
 }
 
-function replace() {
-  var index1 = actionList.indexOf("Docked (T)");
-  var index2 = actionList.indexOf("Docked & Engaged (T)");
-  var index3 = actionList.indexOf("Parked (T)");
-  
 
-  if (index1 > -1) {
-    actionList.splice(index1, 1);
-  }
-  if (index2 > -1) {
-    actionList.splice(index2, 1);
-  }
-  if (index3 > -1) {
-    actionList.splice(index3, 1);
-  }
-  
 
-  
-  var compressed2 = compressedList.indexOf(19);
-  var compressed3 = compressedList.indexOf(20);
-  var compressed1 = compressedList.indexOf(21);
-
-  
-  
-  if (compressed2 > -1) {
-    compressedList.splice(compressed2, 1);
-  }
-  if (compressed3 > -1) {
-    compressedList.splice(compressed3, 1);
-  }
-  if (compressed1 > -1) {
-    compressedList.splice(compressed1, 1);
-  }
-
-  console.log(actionList);
-  updateLog();
-}
-
-function score(){
-  var Foul =  compressedList.filter(item => item === 1).length;
-  var AutonSpeaker = compressedList.filter(item => item === 2).length;
-  var AutonSpeakerMiss = compressedList.filter(item => item === 3).length;
-  var AutonAmp = compressedList.filter(item => item === 4).length;
-  var AutonAmpMiss = compressedList.filter(item => item === 5).length;
-  var TeleopSpeaker = compressedList.filter(item => item === 6).length;
-  var TeleopSpeakerMiss =  compressedList.filter(item => item === 7).length;
-  var TeleopAmp = compressedList.filter(item => item === 8).length;
-  var TeleopAmpMiss = compressedList.filter(item => item === 9).length;
-  var Trap = compressedList.filter(item => item === 10).length;
-  var Climb = compressedList.filter(item => item === 11).length;
-  var FailedClimb =  compressedList.filter(item => item === 12).length;
-  var Disabled =  compressedList.filter(item => item === 13).length;
-  var Defence = compressedList.filter(item => item === 14).length;
-
-  var autonScore = (2 * AutonAmp + 5 * AutonSpeaker)
-  var teleopScore = (TeleopAmp + 3 * TeleopSpeaker)
-  var endgameScore = (5 * Trap) + (3 * Climb)
-  var scorer =  autonScore + teleopScore + endgame;
-document.getElementById("score").value = scorer;
-console.log(scorer);
-}
-
-function failedDocked() {
-  var index7 = actionList.indexOf("Docked (T)");
-  var index8 = actionList.indexOf("Docked & Engaged (T)");
-  var index9 = actionList.indexOf("Failed to Dock (T)");
+function replaceFail() {
+  var index7 = actionList.indexOf("Climb");
+  var index8 = actionList.indexOf("Failed Climb");
   
 
   if (index7 > -1) {
@@ -126,15 +68,11 @@ function failedDocked() {
   if (index8 > -1) {
     actionList.splice(index8, 1);
   }
-  if (index9 > -1) {
-    actionList.splice(index9, 1);
-  }
   
 
   
-  var compressed7 = compressedList.indexOf(18);
-  var compressed8 = compressedList.indexOf(20);
-  var compressed9 = compressedList.indexOf(21);
+  var compressed7 = compressedList.indexOf(11);
+  var compressed8 = compressedList.indexOf(12);
 
   
   
@@ -144,55 +82,25 @@ function failedDocked() {
   if (compressed7 > -1) {
     compressedList.splice(compressed8, 1);
   }
-  if (compressed9 > -1) {
-    compressedList.splice(compressed9, 1);
-  }
 
   console.log(actionList);
   updateLog();
 
 }
-function replaceGrid() {
-var gridfull = actionList.indexOf("Grid Full");
-var gridcompressed = compressedList.indexOf(23);
-if (gridfull > -1) {
-  actionList.splice(gridfull, 1);
+function Defense() {
+var defender = actionList.indexOf("Defense");
+var defendercompressed = compressedList.indexOf(16);
+if (defender > -1) {
+  actionList.splice(defender, 1);
 }
-if (gridcompressed > -1) {
-  compressedList.splice(gridcompressed, 1);
-}
-
-
+if (defendercompressed > -1) {
+  compressedList.splice(defendercompressed, 1);
 }
 
-function replaceAuton() {
-  var index5 = actionList.indexOf("Docked (A)");
-  var index6 = actionList.indexOf("Docked & Engaged (A)");
-  var index10 = actionList.indexOf("Failed to Dock (A)")
-  var compressed5 = compressedList.indexOf(3);
-  var compressed6 = compressedList.indexOf(4);
-  var compressed10 = compressedList.indexOf(22);
 
-  if (index5 > -1) {
-    actionList.splice(index5, 1);
-  }
-  if (index6 > -1) {
-    actionList.splice(index6, 1);
-  }
-  if (index10 > -1) {
-    actionList.splice(index10, 1);
-  }
-
-  if (compressed5 > -1) {
-    compressedList.splice(compressed5, 1);
-  }
-  if (compressed6 > -1) {
-    compressedList.splice(compressed6, 1);
-  }
-  if (compressed10 > -1) {
-    compressedList.splice(compressed10, 1);
-  }
 }
+
+
 
 function placebets() {
   var red = actionList.indexOf("Red Alliance");
@@ -205,24 +113,6 @@ function placebets() {
   console.log(actionList);
   //console.log(compressedList);
 }
-//the code below is jackson testing something with grouping consecutitive button presses, save please :) 
-/* function addActionClasic(action, number) { //Used for buttons that have a data validation script
-    actionList.push(action); //Add it to the actionList (what the scouter sees on the app)
-    compressedList.push(number); //Add it to the compressedList (QR Code)//
-    updateLog(); //Update what the scouter sees on the app (actionList)
-}
-function addAction(action, number) {  //The New one
-    curentAction = action; //Sets the curent action as a variable to be refrenced in the theSame function
-    const result = actionList.filter(theSame); //This sorts the array into a "copy array" with only values that equal curentAction
-    var times = result.length + 1; //Get the number of times the action shows up by getting the length of the results
-    var push = action + " X" + times; //Makes the text the scouter sees
-    actionList.push(push); //Add it to the actionList (what the scouter sees on the app)
-    compressedList.push(number); //Add it to the compressedList (QR Code)//
-    updateLog(); //Update what the scouter sees on the app (actionList)
-}
-function theSame(action) {
-    return action.includes(curentAction); //Checks if the curent action matches a value in actionList
-} */ 
 function addAction(action, number) { //Used for buttons that have a data validation script
     actionList.push(action); //Add it to the actionList (what the scouter sees on the app)
     compressedList.push(number); //Add it to the compressedList (QR Code)//
@@ -230,7 +120,7 @@ function addAction(action, number) { //Used for buttons that have a data validat
 }
 
 
-function addIndexInfo(action) {
+function setAlliance(action) {
   actionList.push(action); //Add it to the actionList (what the scouter sees on the app)
 
   extraData[4] = action; //Add it to the compressedList (QR Code)//
@@ -317,13 +207,6 @@ function pullArrayQR() {
   document.getElementById("teamnum").value = extraData[0];
   document.getElementById("matchnum").value = extraData[1];
 }
-function secret() {
-  document.getElementById("auton").style.opacity = "0";
-  document.getElementById("anton").style.opacity = "100";
-  sessionStorage.setItem("ActionList", JSON.stringify(actionList));
-  sessionStorage.setItem("qrlist", JSON.stringify(compressedList));
-  sessionStorage.setItem("extraData", JSON.stringify(extraData));
-}
 
 function commentBox() {
   extraData[3] = document.getElementById("comments").value;
@@ -332,13 +215,14 @@ function commentBox() {
 
 function secret() {
   document.getElementById("auton").style.opacity = "0";
+  document.getElementById("anton").style.opacity = "100";
 }
 
 function reset() {
-  if (confirm("If you could, taking a screenshot would be the right thing to do! :)")) {
+  if (confirm("If you could, taking a screenshot would be the right thing to do! :)") == true) {
     sessionStorage.removeItem("ActionList");
     sessionStorage.removeItem("qrlist");
-    incmatchnumber++
+    incmatchnumber++ //increses the variable by one
     sessionStorage.setItem("matchnumber", incmatchnumber);
     window.location.href = "./index.html";
   }
@@ -358,14 +242,15 @@ function GO(team, match, scout) {
     alert("Enter you initials!");
     go = false;
   }
-  if (go) {
+  if (go == true) {
     extraData[0] = team;
     extraData[1] = match;
     extraData[2] = scout;
     incmatchnumber = match;
     sessionStorage.setItem("ScoutInitials", scout);
     sessionStorage.setItem("matchnumber", incmatchnumber);
-    window.location.href = "./newAuton.html";
+    saveArray();
+    window.location.href = "./Auton.html";
   }
 }
 
@@ -376,7 +261,7 @@ function blueall() {
   document.querySelector(".grid-container").style.backgroundColor = "blue";
 }
 function setipadid(id) {
-    if (rUsure) {
+    if (rUsure ==  true) {
         ipadID = id;
         console.log(ipadID);
         localStorage.setItem("iPadId", ipadID);
